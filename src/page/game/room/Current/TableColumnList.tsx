0c9d142da448de62376267c9ef763ad30ee160d1
caoguiming <1677664380@qq.com>
2022-10-10 19:18:17 +0800

    【i】【CmcOrg】

--- src/page/game/room/Current/TableColumnList.tsx
+++ src/page/game/room/Current/TableColumnList.tsx
@@ -1,6 +1,12 @@
 import {ActionType, ProColumns} from "@ant-design/pro-components";
 import {GameRoomCurrentDeleteByIdSet, GameRoomCurrentPageVO} from "@/api/admin/GameRoomCurrentController";
 import {ExecConfirm, ToastSuccess} from "@/util/ToastUtil";
+import {
+    GameMoneyTypeEnumSelectList,
+    GameRoomConfigPlayTypeEnumSelectList,
+    GameRoomConfigRoomTypeEnumSelectList,
+    GameUserExpTypeEnumSelectList
+} from "@/page/game/room/Config/Enums";
 
 const TableColumnList = (actionRef: React.RefObject<ActionType>): ProColumns<GameRoomCurrentPageVO>[] => [
     {
@@ -11,25 +17,67 @@ const TableColumnList = (actionRef: React.RefObject<ActionType>): ProColumns<Gam
 
     {title: '主键 id', dataIndex: 'id', ellipsis: true,},
 
-    {title: '房间配置主键 id', dataIndex: 'roomConfigId', ellipsis: true,},
+    {title: '配置id', dataIndex: 'roomConfigId', ellipsis: true,},
 
-    {title: '房间配置名称', dataIndex: 'name', ellipsis: true,},
+    {title: '配置名称', dataIndex: 'name', ellipsis: true,},
 
-    {title: '房间玩法：1 大厅 2 捕鱼 3 斗地主', dataIndex: 'playType', ellipsis: true,},
+    {
+        title: '房间玩法', dataIndex: 'playType', valueType: 'select',
+        fieldProps: {
+            showSearch: true,
+            options: GameRoomConfigPlayTypeEnumSelectList,
+        },
+    },
 
-    {title: '房间类型，例如：1000 普通大厅 2000 体验场 2001 普通场 2002 挑战场 2003 大奖赛', dataIndex: 'roomType', ellipsis: true,},
+    {
+        title: '房间类型', dataIndex: 'roomType', valueType: 'select',
+        fieldProps: {
+            showSearch: true,
+            options: GameRoomConfigRoomTypeEnumSelectList,
+        },
+    },
 
-    {title: '消耗货币类型：1 金币 2 龙晶 3 钻石 4 临时货币（退房间清零）', dataIndex: 'useMoneyType', ellipsis: true,},
+    {
+        title: '消耗货币', dataIndex: 'useMoneyType', valueType: 'select',
+        fieldProps: {
+            showSearch: true,
+            options: GameMoneyTypeEnumSelectList,
+        },
+    },
 
-    {title: '得到货币类型：1 金币 2 龙晶 3 钻石 4 临时货币（退房间清零）', dataIndex: 'gotMoneyType', ellipsis: true,},
+    {
+        title: '得到货币', dataIndex: 'gotMoneyType', valueType: 'select',
+        fieldProps: {
+            showSearch: true,
+            options: GameMoneyTypeEnumSelectList,
+        },
+    },
 
-    {title: '用户限制货币类型：1 金币 2 龙晶 3 钻石 4 临时货币（退房间清零）', dataIndex: 'limitMoneyType', ellipsis: true,},
+    {
+        title: '限制货币', dataIndex: 'limitMoneyType', valueType: 'select',
+        fieldProps: {
+            showSearch: true,
+            options: GameMoneyTypeEnumSelectList,
+        },
+    },
 
-    {title: '用户限制经验值类型：1 普通经验', dataIndex: 'limitExpType', ellipsis: true,},
+    {
+        title: '限制经验', dataIndex: 'limitExpType', valueType: 'select',
+        fieldProps: {
+            showSearch: true,
+            options: GameUserExpTypeEnumSelectList,
+        },
+    },
 
-    {title: '房间增加经验值的类型：1 普通经验', dataIndex: 'roomExpType', ellipsis: true,},
+    {
+        title: '增加经验', dataIndex: 'roomExpType', valueType: 'select',
+        fieldProps: {
+            showSearch: true,
+            options: GameUserExpTypeEnumSelectList,
+        },
+    },
 
-    {title: 'socket服务器主键 id', dataIndex: 'socketServerId', ellipsis: true,},
+    {title: '服务器id', dataIndex: 'socketServerId', ellipsis: true,},
 
     {
         title: '操作',
