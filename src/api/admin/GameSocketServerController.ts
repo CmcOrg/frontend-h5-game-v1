46b368612784534f5039250d11d23ba0a00bc6ab
caoguiming <1677664380@qq.com>
2022-10-11 09:39:13 +0800

    【i】【CmcOrg】

---
+++ src/api/admin/GameSocketServerController.ts
@@ -0,0 +1,52 @@
+import {SortOrder} from "antd/es/table/interface";
+import MyOrderDTO from "@/model/dto/MyOrderDTO";
+import $http from "@/util/HttpUtil";
+import {AxiosRequestConfig} from "axios";
+
+export interface NotEmptyIdSet {
+    idSet: number[] // 主键 idSet
+}
+
+// 批量删除
+export function GameSocketServerDeleteByIdSet(form: NotEmptyIdSet, config?: AxiosRequestConfig) {
+    return $http.myPost<string>('/game/socketServer/deleteByIdSet', form, config)
+}
+
+export interface NotNullId {
+    id: number // 主键id {"min":1}
+}
+
+export interface GameSocketServerDO {
+    ip?: string // ip
+    port?: number // 端口
+    maxConnect?: number // 最大连接数
+    id?: number // 主键id
+    createId?: number // 创建人id
+    createTime?: string // 创建时间
+    updateId?: number // 修改人id
+    updateTime?: string // 修改时间
+    version?: number // 乐观锁
+    enableFlag?: boolean // 是否启用
+    delFlag?: boolean // 是否逻辑删除
+    remark?: string // 备注
+}
+
+// 通过主键id，查看详情
+export function GameSocketServerInfoById(form: NotNullId, config?: AxiosRequestConfig) {
+    return $http.myProPost<GameSocketServerDO>('/game/socketServer/infoById', form, config)
+}
+
+export interface GameSocketServerPageDTO {
+    ip?: string // ip
+    port?: number // 端口
+    remark?: string // 备注
+    current?: number // 第几页
+    pageSize?: number // 每页显示条数
+    order?: MyOrderDTO // 排序字段
+    sort?: Record<string, SortOrder> // 排序字段（只在前端使用，实际传值：order）
+}
+
+// 分页排序查询
+export function GameSocketServerPage(form: GameSocketServerPageDTO, config?: AxiosRequestConfig) {
+    return $http.myProPagePost<GameSocketServerDO>('/game/socketServer/page', form, config)
+}
---
+++ src/page/game/socket/Server/TableColumnList.tsx
@@ -0,0 +1,42 @@
+import {YesNoDict} from "@/util/DictUtil";
+import {ActionType, ProColumns} from "@ant-design/pro-components";
+import {GameSocketServerDO} from "@/api/admin/GameSocketServerController";
+
+const TableColumnList = (actionRef: React.RefObject<ActionType>): ProColumns<GameSocketServerDO>[] => [
+    {
+        title: '序号',
+        dataIndex: 'index',
+        valueType: 'index',
+    },
+
+    {title: 'ip', dataIndex: 'ip', ellipsis: true,},
+
+    {title: '端口', dataIndex: 'port', ellipsis: true,},
+
+    {title: '最大连接数', dataIndex: 'maxConnect', ellipsis: true,},
+
+    {
+        title: '创建时间',
+        dataIndex: 'createTime',
+        hideInSearch: true,
+        valueType: 'fromNow',
+    },
+
+    {
+        title: '修改时间',
+        dataIndex: 'updateTime',
+        hideInSearch: true,
+        valueType: 'fromNow',
+    },
+
+    {
+        title: '是否启用',
+        dataIndex: 'enableFlag',
+        valueEnum: YesNoDict
+    },
+
+    {title: '备注', dataIndex: 'remark', ellipsis: true,},
+
+];
+
+export default TableColumnList
